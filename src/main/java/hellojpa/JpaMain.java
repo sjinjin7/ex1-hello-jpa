cf83d5d49777f158be7bfd774df35c1de190de38
KimSeoJin <sjinjin7@gmail.com>
2022-07-26 22:55:43 +0900

    영속성 컨텍스트2 & 플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,17 +16,41 @@ public class JpaMain {
         tx.begin();
 
         try{
-
+/**
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             //영속
             System.out.println("== BEFORE ==");
             em.persist(member);
             System.out.println("== AFTER ==");
-            
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+ **/
+/**
+
+
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+ */
+/**
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=======================");
+ */
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            System.out.println("=======================");
 
             tx.commit();
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,9 +6,24 @@ import javax.persistence.Id;
 @Entity
 public class Member {
 
+
+
+
+
+
     @Id
     private Long id;
     private String name;
+
+    //JPA는 엔티티 객체에 리플렉션과 같은 작업을 해야 하기 때문에 기본 생성자가 반드시 존재 해야 한다.
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 
     public Long getId() {
         return id;
