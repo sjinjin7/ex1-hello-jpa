87b63494efde1a237cdc01fb879919626f5d27e4
KimSeoJin <sjinjin7@gmail.com>
2022-08-11 03:36:07 +0900

    영속성 전이 & 고아객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -184,7 +184,7 @@ public class JpaMain {
              **/
 
             /*======*/
-
+            /**
             Team team = new Team();
             team.setName("teamA");
             em.persist(team);
@@ -203,11 +203,27 @@ public class JpaMain {
             System.out.println("=========");
             m.getTeam().getName();
             System.out.println("=========");
+            **/
 
+            /*========*/
 
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
-            System.out.println("=====");
+
         } catch (Exception e){
             tx.rollback();
             e.printStackTrace();
